9587555a0824687421e449c66d1d4e3b54ec7f54
Rishabh sharma <rishabhstar427@gmail.com>
2021-06-29 20:19:01 +0530

    changed the style sheet and javascript file

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r= document.getElementById("rate").value;
+    y=document.getElementById("years").value;
+    
+    res=p*r*y
+    document.getElementById("result").innerHTML=res-p
+    document.getElementById("resul").innerHTML="If you deposit<br>"+ res+"at an interest rate of"+r+"% <br>"+"You will recieve an amount of <br>"+res ;
     
 }
         
