73079c028061feaee4ac3d32b62ecc39617474fd
Rishabh sharma <rishabhstar427@gmail.com>
2021-07-04 10:39:21 +0530

    changed index css and javascript file

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ function compute()
     r= document.getElementById("rate").value;
     y=document.getElementById("years").value;
     if(p<=0){
-        alert("please enter vale greater then zero");
+        alert("please enter a positive number ");
         setTimeout(function(){
                 document.getElementById("principal").focus();
                 
             }, 1);
     }
-    let cur=2021;
+    else{let cur=2021;
     
     res=(p*r/100)*y;
     let z=cur+parseInt(y);
-    document.getElementById("result").innerHTML=res;
-    document.getElementById("resul").innerHTML="<br>If you deposit  "+p+"<br>at an interest rate of  "+r+"% <br>"+"You will recieve an amount of "+res+"<br>in the Year "+z;
     
+    document.getElementById("result").innerHTML=res;
+    document.getElementById("resul").innerHTML="<br>If you deposit "+p+"<br>at an interest rate of  "+r+"% <br>"+"You will recieve an amount of "+res+"<br>in the Year "+z;
+    }
 }
         
